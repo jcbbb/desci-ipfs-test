73505d568d0738ffb0f4d150b1d0bfb81393ce13
jcbbb <avaz28082000@gmail.com>
2023-07-23 18:42:20 +0500

    I don't know. Maybe working :(

--- pages/api/ipfs.ts
+++ pages/api/ipfs.ts
@@ -1,8 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { strings } from '@helia/strings';
+import { gossipsub } from '@chainsafe/libp2p-gossipsub';
+import { kadDHT } from '@libp2p/kad-dht';
+import { createLibp2p } from 'libp2p';
+import { tcp } from "@libp2p/tcp";
+import { createHelia } from 'helia';
+import { ipns, ipnsValidator, ipnsSelector } from '@helia/ipns';
+import { dht, pubsub } from '@helia/ipns/routing';
+import { unixfs } from '@helia/unixfs';
+import { dagCbor } from "@helia/dag-cbor";
 
-import { createHelia } from 'helia'
-import { strings } from '@helia/strings'
+let helia = await createHelia();
+let name = ipns(helia);
+
+let strfs = strings(helia);
+let cborfs = dagCbor(helia);
+let keyinfo = await helia.libp2p.keychain.createKey("my-key", "RSA");
+let peerId = await helia.libp2p.keychain.exportPeerId(keyinfo.name);
 
 // arbitrary response format
 export type BasicIpfsData = {
@@ -15,6 +30,7 @@ export default async function handler(
   res: NextApiResponse<BasicIpfsData>
 ) {
   if (req.method === "POST") {
+    insertData(req, res);
     // Process a POST request
   } else {
     // Handle any other HTTP method
@@ -22,17 +38,28 @@ export default async function handler(
   }
 }
 
+const insertData = async (req: NextApiRequest, res: NextApiResponse) => {
+  let { content } = req.body;
+  let cid = await name.resolve(peerId).catch(console.error)
+  if (!cid) cid = await cborfs.add([]);
+
+  let data = await cborfs.get(cid);
+  let strcontent = await strfs.add(content);
+  data.push({ cid: strcontent.toString(), content })
+  const result = await cborfs.add(data);
+
+  await name.publish(peerId, result);
+  res.status(201).json({ cid: strcontent.toString(), content });
+}
+
 const retrieveData = async (
   req: NextApiRequest,
   res: NextApiResponse<BasicIpfsData>
 ) => {
-  // connect to the default API address http://localhost:5001
-  // call Core API methods
-  const helia = await createHelia()
-  const strfs = strings(helia)
-  const string = "Hello world!";
-  const result = await strfs.add(string);
-
-
-  res.status(200).json({ cid: result.toString(), content: string });
+  let existingCid = await name.resolve(peerId).catch(console.error)
+  let notes = []
+  if (existingCid) {
+    notes = await cborfs.get(existingCid);
+  }
+  res.status(200).json(notes);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,37 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { BasicIpfsData } from "./api/ipfs";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
-  const [result, setResult] = useState<BasicIpfsData | null>(null);
+  const [results, setResults] = useState<BasicIpfsData[]>([]);
   const [note, setNote] = useState<BasicIpfsData | null>(null);
   const [txt, setTxt] = useState("");
+  const inputRef = useRef<HTMLTextareaElement>(null);
 
   const handleLoad = async () => {
     setLoading(true);
-
     const { data } = await axios.get("/api/ipfs");
-    console.log({ data });
-    setResult(data);
+    setResults(data);
     setLoading(false);
   };
 
   // avoiding ternary operators for classes
   function classNames(...classes: any) {
     return classes.filter(Boolean).join(" ");
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const values = Object.fromEntries(new FormData(e.target));
+    const { data } = await axios.post("/api/ipfs", values);
+    setResults((prev) => [...prev, data])
+    e.target.reset();
+    if (inputRef.current) inputRef.current.focus();
   }
 
   return (
@@ -34,25 +42,35 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main class="max-w-7xl mx-auto w-full">
         <div className="flex flex-col gap-3">
-          <div className="text-3xl font-bold underline">IPFS Notes</div>
-          {!!result ? (
-            <div className="flex flex-col">
-              <span>Content: {result.content}</span>
-              <span>CID: {result.cid}</span>
-            </div>
-          ) : null}
-          <div>
-            <button
-              onClick={handleLoad}
-              className={classNames(
-                "bg-slate-300 hover:bg-slate-500 text-black rounded-md p-2 drop-shadow-md w-32",
-                loading ? "animate-pulse" : ""
-              )}
-            >
-              {loading ? "Loading..." : "Retrieve Data"}
-            </button>
+          <h1 className="text-3xl font-bold underline">IPFS Notes</h1>
+          <div className="flex">
+            <section className="max-w-lg w-full shrink-0 break-words">
+              {results.map((result, idx) => (
+                <div className="flex flex-col" key={idx}>
+                  <span>Content: {result.content}</span>
+                  <span>CID: {result.cid}</span>
+                </div>
+              ))}
+              <div>
+                <button
+                  onClick={handleLoad}
+                  className={classNames(
+                    "bg-slate-300 hover:bg-slate-500 text-black rounded-md p-2 drop-shadow-md w-32",
+                    loading ? "animate-pulse" : ""
+                  )}
+                >
+                  {loading ? "Loading..." : "Retrieve Data"}
+                </button>
+              </div>
+            </section>
+            <section className="w-full">
+              <form className="flex flex-col space-y-2 max-w-sm" onSubmit={onSubmit}>
+                <textarea required minlength="1" ref={inputRef} name="content" className="border border-slate-300 p-2 text-normal"></textarea>
+                <button className="bg-slate-300 hover:bg-slate-500 text-black rounded-md p-2 drop-shadow-md">Create</button>
+              </form>
+            </section>
           </div>
         </div>
       </main>
